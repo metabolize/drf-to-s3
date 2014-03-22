c8ad686cda9c68b4fd3fff31f7d480a950444bbb
Paul Melnikow <paul.melnikow@bodylabs.com>
2014-03-22 07:17:57 -0400

    Bump this to Alpha

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     ],
     install_requires=install_requires,
     classifiers = [
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
